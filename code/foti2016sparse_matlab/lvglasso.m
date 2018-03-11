b4fa17a81aac439e834b0f0a426e4af05a459f2a
smanivasagam3 <smanivasagam3@gatech.edu>
2018-03-11 12:25:14 -0400

    Add lvglasso and updated glasso drafts

--- code/foti2016sparse_matlab/lvglasso.m
+++ code/foti2016sparse_matlab/lvglasso.m
@@ -1,1 +1,28 @@
-% sparse + low rank graphical lasso (foti et. al 2016, [5])
+% sparse + low rank graphical lasso (ma et. al 2013, [eq 1.2])
+% sigma_hat - sample covariance matrix [p x p]
+% lambda -  L1-norm regularization for sparsity
+% S - Predicted observed precision matrix
+% L - Predicted hidden precision matrix
+% info - CVX debug information
+
+function [S, L , info] = lvglasso(sigma_hat, alpha, beta)
+
+  p = size(sigma_hat);
+  triu_mask = logical(ones(p)-tril(ones(p)));
+  cvx_begin 
+    variable S(p,p) complex symmetric semidefinite
+    variable L(p,p) complex symmetric semidefinite 
+    minimize ( trace((S - L) * sigma_hat) - log_det(S - L) ...
+      + alpha*norm(S(triu_mask),1) + beta*trace(L))  
+  cvx_end
+  
+  obj = [trace((S - L) * sigma_hat) - log_det(S - L) ...
+      + alpha*norm(S(triu_mask),1) + beta*trace(L)];
+
+  info = struct( ...
+    'obj',         obj, ...
+    'cvx_status',  cvx_status, ...
+    'cvx_slvtol',  cvx_slvtol, ...
+    'cvx_cputime', cvx_cputime);
+
+end
