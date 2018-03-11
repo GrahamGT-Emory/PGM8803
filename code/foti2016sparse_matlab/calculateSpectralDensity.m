5ad8b11d6e1ed197623e67f8a8934439b558ecb9
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2018-03-11 12:10:35 -0400

    completed calculation of cross-spectral density

--- code/foti2016sparse_matlab/calculateSpectralDensity.m
+++ code/foti2016sparse_matlab/calculateSpectralDensity.m
@@ -3,40 +3,23 @@
 % --- parameters ---
 F = T;
 useFFT = true;
-ww = hann(F); % TODO - in freq domain?, also, tune length
+ww = hann(F); % TODO - tune length
 
+% --- calculate DFTs ---
+D = 1/sqrt(T)*fft(X,[],2);
 
-% --- calculate periodogram I(wk) ---
-tic;
-kk = 0:T-1;
-tt = 0:T-1;
+% --- calculate periodogram ---
 I = zeros(p,p,F);
-S = zeros(p,p,F);
 for i = 1:p
-  % calculate DFT dd (T x 1)
-  if useFFT
-    dd = 1/sqrt(T)*shiftdim(fft(X(i,:)),1);
-  else
-    dd = zeros(T,1);
-    for ik = 1:T
-      k = kk(ik);
-      for it = 1:T
-        t = tt(it);
-        wk = 2*pi*k/T;
-        dd(ik) = dd(ik) + 1/sqrt(T)*X(i,it)*exp(-1j*wk*t);
-      end
-    end
+  for j = 1:p
+    I(i,j,:) = D(i,:) .* conj(D(j,:));
   end
-  % calculate periodogram
-  I(:,:,i) = 1/(2*pi)*dd.*conj(dd);
 end
 
+% --- calculate smoothed periodogram ---
+S = I .* reshape(ww/sum(ww),[1 1 F]);
+Psi = S;
 
-% --- calculate smoothed periodogram
-for i = 1:p
-  S(:,:,i) = ww.*I(:,:,i);
-end
-
-
-
-
+% notation from Foti et al. 2016:
+%  - Psi[f]      is Psi(:,:,f)
+%  - Psi[.]_{ij} is Psi(i,j,:)
